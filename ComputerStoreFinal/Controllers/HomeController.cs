db470c4710fd6a753cfb869e7548bb30cf039c09
Nathaniel Davis <nwdavis88@gmail.com>
2014-11-19 14:25:11 -0600

    Testing git credential clearing

--- ComputerStoreFinal/Controllers/HomeController.cs
+++ ComputerStoreFinal/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace ComputerStoreFinal.Controllers
 {
+    //test comment for Git testing
     public class HomeController : Controller
     {
         public ActionResult Index()
