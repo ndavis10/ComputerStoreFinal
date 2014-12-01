624825a4ef71798c4cba9c3c868383213d639536
Nathaniel Davis <nwdavis88@gmail.com>
2014-12-01 14:56:51 -0600

    I am an entity framework god.

---
+++ ComputerStoreFinal/DataContexts/ComputerStoreMigrations/201412012044294_ThingsWontWork.cs
@@ -0,0 +1,16 @@
+namespace ComputerStoreFinal.DataContexts.ComputerStoreMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ThingsWontWork : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
