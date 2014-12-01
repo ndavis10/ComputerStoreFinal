915fd6b2ca8e4422282b467ce6ba5a312d6dab26
Nathaniel Davis <nwdavis88@gmail.com>
2014-12-01 13:19:21 -0600

    Added initial ComputerStore migration

---
+++ ComputerStoreFinal/DataContexts/ComputerStoreMigrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ComputerStoreFinal.DataContexts.ComputerStoreMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ComputerStoreFinal.DataContexts.ComputerStoreDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DataContexts\ComputerStoreMigrations";
+        }
+
+        protected override void Seed(ComputerStoreFinal.DataContexts.ComputerStoreDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
