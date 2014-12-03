c9ee35066423482cf0be1a0d61fffea7a599adbe
Nathaniel Davis <nwdavis88@gmail.com>
2014-12-03 14:03:31 -0600

    Merge pull request #25 from mdeboer1

---
+++ ComputerStoreFinal/Controllers/PartsManagerController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ComputerStoreFinal.DataContexts;
+using ComputerStoreFinal.Models;
+
+namespace ComputerStoreFinal.Controllers
+{
+    public class PartsManagerController : Controller
+    {
+        private ComputerStoreDb db = new ComputerStoreDb();
+
+        // GET: PartsManager
+        public ActionResult Index()
+        {
+            return View(db.Parts.ToList());
+        }
+
+        // GET: PartsManager/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = db.Parts.Find(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // GET: PartsManager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PartsManager/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PartID,CategoryID,ModelNumber,PartName,PartDescription,PartPrice,PartImg")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parts.Add(part);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(part);
+        }
+
+        // GET: PartsManager/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = db.Parts.Find(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: PartsManager/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PartID,CategoryID,ModelNumber,PartName,PartDescription,PartPrice,PartImg")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(part).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(part);
+        }
+
+        // GET: PartsManager/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = db.Parts.Find(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: PartsManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Part part = db.Parts.Find(id);
+            db.Parts.Remove(part);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
