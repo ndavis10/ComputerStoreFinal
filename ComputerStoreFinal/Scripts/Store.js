c94ae467f37dc65d37630521a362e35f0783d136
Nathaniel Davis <nwdavis88@gmail.com>
2014-12-10 14:56:21 -0600

    Trying to do javascript stuff

---
+++ ComputerStoreFinal/Scripts/Store.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $(".dropDown").change(function () {
+        var id = $(this).attr('id');
+        var val = $(this).val();
+        $("#" + id + "Desc").text = $("#" + val + "Desc").val();
+    })
+});
