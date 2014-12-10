8ac7d24e1507492c176824e41da076bdb175da31
Nathaniel Davis <nwdavis88@gmail.com>
2014-12-10 15:44:11 -0600

    Renamed images, added images to store index

--- ComputerStoreFinal/Scripts/Store.js
+++ ComputerStoreFinal/Scripts/Store.js
@@ -3,6 +3,8 @@
         var id = $(this).attr('id');
         var val = $(this).val();
         $("#" + id + "Desc").text($("#" + val + "Desc").text());
+        $("#" + id + "Img").attr('src', $("#" + val + "Img").text())
+        $("#" + id + "Img").removeAttr('hidden');
     })
 
     $(".dropDown").trigger('change');
